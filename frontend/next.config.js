ce8cc6214cf4f44296bc37789edf5d632ba534e6
Lucas Joos <joos@dbvis.inf.uni-konstanz.de>
2025-05-01 10:10:39 +0200

    Updating packages & fixing warning

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,14 +1,25 @@
 /** @type {import('next').NextConfig} */
 
 module.exports = {
-    devIndicators: false,
+  devIndicators: false,
 
-    async rewrites() {
-      return [
-        {
-          source: '/api/:path*',
-          destination:  process.env.BACKEND_URL + '/:path*' // proxy to backend
-        }
-      ];
+  // Disable source map generation for both server and client in production
+  productionBrowserSourceMaps: false,
+
+  webpack(config, { isServer, dev }) {
+    // Only modify the config in production
+    if (!dev) {
+      config.devtool = false; // disables source maps completely
     }
-  };
+    return config;
+  },
+
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: process.env.BACKEND_URL + '/:path*', // proxy to backend
+      },
+    ];
+  },
+};
