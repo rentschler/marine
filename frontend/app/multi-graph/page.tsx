47b312f8a8cad19417039937661f6aa278dc3ee5
Jonathan Rentschler <Jonathan.rentschler@uni-konstanz.de>
2025-05-19 16:19:09 +0200

    feat: add layout to the multigraph

--- frontend/app/multi-graph/page.tsx
+++ frontend/app/multi-graph/page.tsx
@@ -11,19 +11,12 @@ const MultiGraphWrapper = dynamic(() => import('@/features/multi-graph/multi-gra
 });
 
 
-
-
-
 export default function Home() {
   return (
-    <div className="h-screen w-full grid grid-cols-[70%_30%] grid-rows-[80%_20%]">
+    <div className="h-screen w-full ">
       <div className=" w-full h-full">
         <MultiGraphWrapper/>
       </div>
-      <div className=" w-full h-full">
-        <FilterDashboard/>
-      </div>
-      <div className="col-span-2 bg-red-500 w-full h-full">Timeline</div>
     </div>
   );
 }
