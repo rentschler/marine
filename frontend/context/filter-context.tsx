b26fca3d109bf0d134c12b7d1fce78edbd1ae41f
rentschler <jonathan.rentschler@uni-konstanz.de>
2025-05-23 12:53:41 +0200

    feat: add date range to filter context

--- frontend/context/filter-context.tsx
+++ frontend/context/filter-context.tsx
@@ -1,40 +1,42 @@
-"use client";
+'use client';
 
-import { FilterContextType } from "@/types/filter-context-type";
-import {createContext, useContext, useState, ReactNode, useEffect} from "react";
+import { FilterContextType } from '@/types/filter-context-type';
+import { createContext, useContext, useState, ReactNode, useEffect } from 'react';
 
 const FilterContext = createContext<FilterContextType | undefined>(undefined);
 
-export function FilterProvider({children}: {children: ReactNode}){
-    const [selectedNodeTypes, setSelectedNodeTypes] = useState<string[]>([]);
-    const [selectedNodeDegrees, setSelectedNodeDegrees] = useState<number[]>([])
-    const [selectedEdgeTypes, setSelectedEdgeTypes] = useState<string[]>([]);
+export function FilterProvider({ children }: { children: ReactNode }) {
+  const [selectedNodeTypes, setSelectedNodeTypes] = useState<string[]>([]);
+  const [selectedNodeDegrees, setSelectedNodeDegrees] = useState<number[]>([]);
+  const [selectedEdgeTypes, setSelectedEdgeTypes] = useState<string[]>([]);
+  const [selectedDateRange, setSelectedDateRange] = useState<Date[]>([]);
 
-    useEffect(()=>{
-        console.log(selectedNodeTypes, selectedNodeDegrees, selectedEdgeTypes);
-    },[selectedNodeTypes, selectedNodeDegrees, selectedEdgeTypes])
+  useEffect(() => {
+    console.log(selectedNodeTypes, selectedNodeDegrees, selectedEdgeTypes);
+  }, [selectedNodeTypes, selectedNodeDegrees, selectedEdgeTypes]);
 
-    return(
-        <FilterContext.Provider
-            value={{
-                selectedNodeTypes,
-                setSelectedNodeTypes,
-                selectedNodeDegrees,
-                setSelectedNodeDegrees,
-                selectedEdgeTypes,
-                setSelectedEdgeTypes,
-            }}
-        >
-          {children}
-        </FilterContext.Provider>
-    )
-
+  return (
+    <FilterContext.Provider
+      value={{
+        selectedNodeTypes,
+        setSelectedNodeTypes,
+        selectedNodeDegrees,
+        setSelectedNodeDegrees,
+        selectedEdgeTypes,
+        setSelectedEdgeTypes,
+        selectedDateRange,
+        setSelectedDateRange,
+      }}
+    >
+      {children}
+    </FilterContext.Provider>
+  );
 }
 
-export function useFilterContext(){
-    const context = useContext(FilterContext);
-    if(!context){
-        throw new Error("useFilterContext must be used within a FilterProvider")
-    }
-    return context;
+export function useFilterContext() {
+  const context = useContext(FilterContext);
+  if (!context) {
+    throw new Error('useFilterContext must be used within a FilterProvider');
+  }
+  return context;
 }
