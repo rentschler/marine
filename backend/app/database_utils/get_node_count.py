a2d41659cd82783408d5d25591cc423c0d3dc560
Jonathan Rentschler <Jonathan.rentschler@uni-konstanz.de>
2025-06-02 11:04:03 +0200

    fix: update Cypher query to count nodes to check if database exists on startup of the backend

--- backend/app/database_utils/get_node_count.py
+++ backend/app/database_utils/get_node_count.py
@@ -1,6 +1,6 @@
 async def get_node_count_in_db(session):
     try:
-        result = await session.run("MATCH (n:Node) RETURN count(n) AS node_count")
+        result = await session.run("MATCH (n) RETURN count(n) AS node_count")
         record = await result.single()
         return record["node_count"]
     except Exception as e:
