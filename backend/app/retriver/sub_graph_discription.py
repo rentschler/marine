811ae76bc3ab67a128e0b6eda1c36b3aca80b23c
Jonathan Rentschler (DBVIS-Students) <Jonathan.rentschler@uni-konstanz.de>
2025-07-14 17:19:45 +0200

    feat: new rag system

--- backend/app/retriver/sub_graph_discription.py
+++ backend/app/retriver/sub_graph_discription.py
@@ -4,6 +4,7 @@ from pydantic import BaseModel
 
 
 class SubGraphDiscription(BaseModel):
+    day: Optional[str] = None
     graph: GraphData
     description: str
     llm_summary: Optional[str] = None
